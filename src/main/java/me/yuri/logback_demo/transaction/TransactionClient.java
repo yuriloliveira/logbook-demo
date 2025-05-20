58628a61e651b83d704082baeec342bfcc5cbf33
Yuri Oliveira <yuri.oliveira@wearewaes.com>
2025-05-20 06:49:23 +0200

    feat(transactions): transaction creation and transaction count on client.

--- src/main/java/me/yuri/logback_demo/transaction/TransactionClient.java
+++ src/main/java/me/yuri/logback_demo/transaction/TransactionClient.java
@@ -3,6 +3,7 @@ package me.yuri.logback_demo.transaction;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
 
 import java.util.List;
 
@@ -15,14 +16,36 @@ public class TransactionClient {
     }
 
     public void createTransaction(TransactionRequest transactionRequest) {
-
+        client
+            .post()
+            .uri("/transactions")
+            .body(transactionRequest)
+            .retrieve()
+            .toBodilessEntity();
     }
 
     public List<Transaction> getTransactionsByIban(String iban) {
+        return getTransactionsByIban(iban, null);
+    }
+
+    public List<Transaction> getTransactionsByIban(String iban, Integer count) {
         return client
                 .get()
-                .uri(uriBuilder -> uriBuilder.path("/transactions").queryParam("iban", iban).build())
+                .uri(uriBuilder ->
+                    withCountQueryParamIfNotNull(uriBuilder, count)
+                        .path("/transactions")
+                        .queryParam("iban", iban)
+                        .build()
+                )
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {});
+    }
+
+    private static UriBuilder withCountQueryParamIfNotNull(UriBuilder uriBuilder, Integer count) {
+        if (count != null) {
+            return uriBuilder.queryParam("count", count);
+        }
+
+        return uriBuilder;
     }
 }
